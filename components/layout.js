acdb506deeff86a3f52c27692e47f7487062bb5f
levy kipkoech <kipkoechlevy096@gmail.com>
2024-01-15 15:32:54 +0300

    add images

--- components/layout.js
+++ components/layout.js
@@ -7,23 +7,27 @@ export default function Layout({ title, children }) {
     <>
       <Head>
         <title>{title ? title + '-Jumiz' : 'Jumiz'}</title>
-        <meta name="description" content="ecommerce website"/>
-        <link rel="icon" href="/flavi.ico"/>
+        <meta name="description" content="ecommerce website" />
+        <link rel="icon" href="/flavi.ico" />
       </Head>
-      <div className='flex min-h-screen flex-col justify-between'>
+      <div className="flex min-h-screen flex-col justify-between">
         <header>
-            <nav className='flex h-12 justify'>
-                <Link href='/'>
-                    <a className='text-lg font-bold'>jumiz</a>
-                </Link>
-                <div>
-                    <Link href='/cart'>Cart</Link>
-                    <Link href='/login'>Login</Link>
-                </div>
-            </nav>
+          <nav className="flex h-12 justify-between px-4 shadow-md items-center">
+            <Link href="/" className="text-lg font-bold">
+              jumiz
+            </Link>
+            <div>
+              <Link href="/cart" className="p-2">
+                cart
+              </Link>
+              <Link href="/login" className="p-2">
+                Login
+              </Link>
+            </div>
+          </nav>
         </header>
-        <main>{children}</main>
-        <footer>footer</footer>
+        <main className="container m-auto mt-4 px-4">{children}</main>
+        <footer className='flex h-10 shadow-inner justify-center items-center'>copyright @ 2024 jumiz</footer>
       </div>
     </>
   );
