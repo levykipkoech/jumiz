2798441077449f5673be7ba47555475e15cc59f6
levy kipkoech <kipkoechlevy096@gmail.com>
2024-01-16 12:30:15 +0300

    added product item

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import Link from 'next/link';
 
 export default function Layout({ title, children }) {
   return (
-    <>
+    <div>
       <Head>
         <title>{title ? title + '-Jumiz' : 'Jumiz'}</title>
         <meta name="description" content="ecommerce website" />
@@ -13,22 +13,24 @@ export default function Layout({ title, children }) {
       <div className="flex min-h-screen flex-col justify-between">
         <header>
           <nav className="flex h-12 justify-between px-4 shadow-md items-center">
-            <Link href="/" className="text-lg font-bold">
-              jumiz
+            <Link href="/" as="/" legacyBehavior>
+              <a className="text-lg font-bold">jumiz</a>
             </Link>
             <div>
-              <Link href="/cart" className="p-2">
-                cart
+              <Link href="/cart" as="/" legacyBehavior>
+                <a className="p-2">cart</a>
               </Link>
-              <Link href="/login" className="p-2">
-                Login
+              <Link href="/login" as="/" legacyBehavior>
+                <a className="p-2">Login</a>
               </Link>
             </div>
           </nav>
         </header>
         <main className="container m-auto mt-4 px-4">{children}</main>
-        <footer className='flex h-10 shadow-inner justify-center items-center'>copyright @ 2024 jumiz</footer>
+        <footer className="flex h-10 shadow-inner justify-center items-center">
+          copyright @ 2024 jumiz
+        </footer>
       </div>
-    </>
+    </div>
   );
 }
